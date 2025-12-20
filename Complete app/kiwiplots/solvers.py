c5e862e67fb548d97d18c876172d409857a6b887
Jan-Novak00 <njan37509@gmail.com>
2025-12-20 16:10:30 +0100

    unified solver constructor calling

--- Complete app/kiwiplots/solvers.py
+++ Complete app/kiwiplots/solvers.py
@@ -2,19 +2,37 @@ from .plotelement import VariableRectangleGroup, VariablePoint2D, VariableCandle
 from .variableplot import VariableChart, VariableBarChart, VariableCandlesticChart
 from kiwisolver import Variable, Constraint, Solver
 from typing import Union
+from abc import ABC, abstractmethod
 
-
-
-
-class ChartSolver:
+class ChartSolver(ABC):
     """
     Encapsulates Solver instance and VariableChart instance.
     """
     def __init__(self):
         self.solver : Solver = Solver()
-        self.variableChart : VariableChart = None
+        self.variableChart : VariableChart = self._initializeVariableChart()
         self.data = None
-        pass
+        self._setConstraints()
+        self._addEditVariables()
+        self._initialSuggest()
+        self.Solve()
+    
+    @abstractmethod
+    def _addEditVariables(self):
+        raise NotImplementedError("Method must be declared in subclass")
+
+    @abstractmethod
+    def _initialSuggest(self):
+        raise NotImplementedError("Method must be declared in subclass")
+
+
+    @abstractmethod
+    def _initializeVariableChart(self) -> VariableChart:
+        raise NotImplementedError("Method must be declared in subclass")
+
+    @abstractmethod
+    def _setConstraints(self):
+        raise NotImplementedError("Method must be declared in subclass")
 
     def Solve(self):
         """
@@ -30,7 +48,7 @@ class ChartSolver:
         self.data = self.variableChart.Value()
     
     def GetOrigin(self):
-        return ValuePoint2D(self.variableChart.origin.X.value(),self.variableChart.origin.Y.value())
+        return self.variableChart.GetOrigin()
 
     def GetSpacing(self):
         return self.variableChart.spacing.value()
@@ -59,40 +77,48 @@ class ChartSolver:
         self.Solve()
 
 
-
-
-
-
 class BarChartSolver(ChartSolver):
     """
     ChartSolver version for bar chart and histogram.
     """
     def __init__(self, width: int, initialHeights: Union[list[int], list[list[int]]], spacing: int, innerSpacing: int, rectangleNames : list[list[str]], xCoordinate: int = 0, yCoordinate: int = 0, widthScalesForGroups : list[list[float]] = None):
+        #Variable capture for correct parent constructor call
+        self.initialHeights = initialHeights
+        self.initialWidth = width
+        self.initialSpacing = spacing
+        self.initialInnerSpacing = innerSpacing
+        self.initialRectangleNames = rectangleNames
+        self.initialxCoordinate = xCoordinate
+        self.initialyCoordinate = yCoordinate
+        self.initialWidthScaleForGroups = widthScalesForGroups
+        #Parent constructor call
         super().__init__()
-        self.variableChart = VariableBarChart(width, initialHeights, spacing, innerSpacing, rectangleNames, xCoordinate, yCoordinate, widthScalesForGroups)
+        #Change of typehinting for variableChart
+        self.variableChart : VariableBarChart = self.variableChart
 
+    def _initializeVariableChart(self) -> VariableChart:
+        return VariableBarChart(self.initialWidth, self.initialHeights, self.initialSpacing, self.initialInnerSpacing, self.initialRectangleNames, self.initialxCoordinate, self.initialyCoordinate, self.initialWidthScaleForGroups)
+
+    def _setConstraints(self):
         barChartConstraints = set(self.variableChart.GetAllConstraints())
-        
         for constraint in barChartConstraints:
             self.solver.addConstraint(constraint)
-
+    
+    def _addEditVariables(self):
         self.solver.addEditVariable(self.variableChart.width, "strong")
         self.solver.addEditVariable(self.variableChart.spacing, "strong")
         self.solver.addEditVariable(self.variableChart.innerSpacing, "strong")
-
-
         for group in self.variableChart.groups:
             for rec in group:
                 self.solver.addEditVariable(rec.height, "strong")
         
         self.solver.addEditVariable(self.variableChart.origin.X, "strong")
         self.solver.addEditVariable(self.variableChart.origin.Y, "strong")
-
         self.solver.addEditVariable(self.variableChart.yAxisHeight, "strong")
-        self.solver.suggestValue(self.variableChart.yAxisHeight, max(max(group) for group in initialHeights)+10)
-        
-        self.Solve()
     
+    def _initialSuggest(self):
+        self.solver.suggestValue(self.variableChart.yAxisHeight, max(max(group) for group in self.initialHeights)+10)
+        
     def SetIntervalValues(self, intervals: list[list[float,float]]):
         self.variableChart.SetIntervalValues(intervals)
 
@@ -128,37 +154,52 @@ class BarChartSolver(ChartSolver):
         self.variableChart.ChangeName(groupIndex,rectangleIndex, newColor)
         self.Update()
 
+
+
 class CandlestickChartSolver(ChartSolver):
     """
     ChartSolver version for candlestick chart.
     """
     def __init__(self, width : int, initialOpening : list[int], initialClosing : list[int], initialMinimum : list[int], initialMaximum : list[int], spacing : int, names : list[str], xCoordinate : int = 0, yCoordinate : int = 0):
+        self.initialWidth = width
+        self.initialOpening = initialOpening
+        self.initialClosing = initialClosing
+        self.initialMinimum = initialMinimum
+        self.initialMaximum = initialMaximum
+        self.initialSpacing = spacing
+        self.initialNames = names
+        self.initialxCoordinate = xCoordinate
+        self.initialyCoordinate = yCoordinate
+        
         super().__init__()
-        self.variableChart : VariableCandlesticChart = VariableCandlesticChart(width,initialOpening,initialClosing,initialMinimum,initialMaximum,spacing,names,xCoordinate,yCoordinate)
+        
+        self.variableChart : VariableCandlesticChart = self.variableChart
 
-        for constraint in self.variableChart.GetAllConstraints():
-            self.solver.addConstraint(constraint)
-
+    def _addEditVariables(self):
         self.solver.addEditVariable(self.variableChart.width, "strong")
         self.solver.addEditVariable(self.variableChart.spacing, "strong")
         self.solver.addEditVariable(self.variableChart.origin.X, "strong")
         self.solver.addEditVariable(self.variableChart.origin.Y, "strong")
-
-        for index, candle in enumerate(self.variableChart.candles):
+        self.solver.addEditVariable(self.variableChart.yAxisHeight, "strong")
+        for candle in self.variableChart.candles:
             self.solver.addEditVariable(candle.height, "strong")
             self.solver.addEditVariable(candle.wickBottom.Y, "strong")
             self.solver.addEditVariable(candle.wickTop.Y, "strong")
             self.solver.addEditVariable(candle.openingCorner.Y, "strong")
 
-            self.solver.suggestValue(candle.wickBottom.Y, initialMinimum[index])
-            self.solver.suggestValue(candle.wickTop.Y, initialMaximum[index])
-            self.solver.suggestValue(candle.openingCorner.Y, initialOpening[index])
+    def _initialSuggest(self):
+        self.solver.suggestValue(self.variableChart.yAxisHeight, max(self.initialMaximum))
+        for index, candle in enumerate(self.variableChart.candles):
+            self.solver.suggestValue(candle.wickBottom.Y, self.initialMinimum[index])
+            self.solver.suggestValue(candle.wickTop.Y, self.initialMaximum[index])
+            self.solver.suggestValue(candle.openingCorner.Y, self.initialOpening[index])
 
-        self.solver.addEditVariable(self.variableChart.yAxisHeight, "strong")
-        self.solver.suggestValue(self.variableChart.yAxisHeight, max(initialMaximum))
-        
-        
-        self.Solve()
+    def _initializeVariableChart(self) -> VariableChart:
+        return VariableCandlesticChart(self.initialWidth, self.initialOpening, self.initialClosing, self.initialMinimum, self.initialMaximum, self.initialSpacing, self.initialNames, self.initialxCoordinate,self.initialyCoordinate)
+
+    def _setConstraints(self):
+        for constraint in self.variableChart.GetAllConstraints():
+            self.solver.addConstraint(constraint)
     
     def ChangeHeight(self, candleIndex : int, height : int):
         self.solver.suggestValue(self.variableChart.candles[candleIndex].height, height)
